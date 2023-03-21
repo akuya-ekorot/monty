0a49a767bcf7caf55aab762b51db18a3b4db0569
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-21 10:45:34 +0300

    open file

--- main.c
+++ main.c
@@ -15,6 +15,27 @@ static void program_usage(int ac)
 }
 
 /**
+ * open_file - opens file passed in as a command line arguement
+ * @path: file to path. part of av
+ *
+ * Return: file stream
+ */
+static FILE *open_file(char *path)
+{
+	FILE *file;
+
+	file = fopen(path, "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", path);
+		exit(EXIT_FAILURE);
+	}
+
+	return (file);
+}
+
+/**
  * main - entry point of the program. A monty interpreter
  * @ac: count of command line arguements
  * @av: list of command line arguements
@@ -23,7 +44,19 @@ static void program_usage(int ac)
  */
 int main(int ac, char **av)
 {
+	FILE *file;
+	char ch;
+
 	program_usage(ac);
+	file = open_file(av[1]);
+
+	do {
+		ch = fgetc(file);
+		printf("%c", ch);
+	} while (ch != EOF);
+
+	fclose(file);
+
 	return (EXIT_SUCCESS);
 }
 
